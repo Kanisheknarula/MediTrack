234bfe8f9178c67f642b0810dddafc11c4749120
neerajsongade25 <neerajsongadeofficial@gmail.com>
2025-11-30 13:31:58 +0530

    the ml changes

--- Meditrack/backend/blockchain/blockchainService.js
+++ Meditrack/backend/blockchain/blockchainService.js
@@ -15,7 +15,8 @@ let contract;
 async function initBlockchain() {
   try {
     // 1) Connect to local Hardhat node
-    provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+    // Fix (V5 syntax)
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL || "http://127.0.0.1:8545");
 
     // 2) Use Hardhat Account #0 PRIVATE KEY (not address!)
     const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".trim();
